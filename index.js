07c7140088314e499ac2dfb782b36d9745b54174
Getye <getye2008@gmail.com>
2024-10-11 08:16:55 +0300

    v3

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const menuRouter = require('./routes/MenuRoute');
 const app = express();
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(cors({
-  origin: 'https://pizza-ordering-app-tau.vercel.app', 
-  credentials: true
-}));
+app.use(cors());
 
 // Set up session middleware
 app.use(session({
