b6c8ec39485e975b502b1f17c98ea93024199cca
Getye <getye2008@gmail.com>
2024-10-11 08:39:33 +0300

    v5

--- dbcon.js
+++ dbcon.js
@@ -1,12 +1,10 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password:"dbpass123",
-    host:"localhost",
-    port: 5432,
-    database:"PizzaOrder"
-
-}) 
+    connectionString: process.env.DATABASE_URL, // This will now use the value from Vercel
+    ssl: {
+      rejectUnauthorized: false // Required for most hosted databases
+    }
+  });
 
 module.exports = pool;
