0d550a6700df488a9b1e751aa9c11e409ce561b3
Getye <getye2008@gmail.com>
2024-10-15 14:47:16 +0300

    profile modal7

--- client/src/components/updateProfile.js
+++ client/src/components/updateProfile.js
@@ -76,7 +76,6 @@ export const UpdateProfile = ({handleProfileModalClose}) => {
         flexDirection: 'column',
         marginLeft: 'auto',
         marginRight: 'auto',
-        maxWidth:'60%'
       }}
     >
       <Box
@@ -134,6 +133,7 @@ export const UpdateProfile = ({handleProfileModalClose}) => {
           fontWeight:'bold', 
           textTransform: 'none', 
           position: 'absolute',    // Make the button stay at a fixed position
+          bottom: 2,              // Adjust the distance from the bottom (16px)
           left: '50%',             // Position the button in the center horizontally
           transform: 'translateX(-50%)'
           }}>Update</Button>
