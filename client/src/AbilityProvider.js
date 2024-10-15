e27fe3e26b85ac8793ea496f9beba4ff5d1daae4
Getye <getye2008@gmail.com>
2024-10-15 11:19:47 +0300

    CASL5

--- client/src/AbilityProvider.js
+++ client/src/AbilityProvider.js
@@ -1,13 +1,20 @@
-import React, { createContext } from 'react';
-import { defineAbilitiesFor } from './permissions'; 
+import React, { createContext, useEffect, useState } from 'react';
+import { defineAbilitiesFor } from './permissions';
 
 export const AbilityContext = createContext(null);
 
 export const AbilityProvider = ({ children }) => {
-  const userRole = localStorage.getItem('userRole'); 
-  const ability = defineAbilitiesFor(userRole); 
-  console.log("Role: ", userRole)
-  console.log("Ability: ", ability)
+  const [ability, setAbility] = useState(defineAbilitiesFor(null)); // Initialize ability
+
+  useEffect(() => {
+    const role = localStorage.getItem('userRole'); // Fetch role from localStorage
+    if (role) {
+      const userAbility = defineAbilitiesFor(role); // Define abilities based on the role
+      setAbility(userAbility); // Update ability state
+      console.log("Role: ", role);  // For debugging
+      console.log("Ability: ", userAbility);  // For debugging
+    }
+  }, []); // Runs only once after the component mounts
 
   return (
     <AbilityContext.Provider value={ability}>
