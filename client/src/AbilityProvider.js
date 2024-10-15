c7994069436d54cfbf6ce7618665c8a5a99c2f82
Getye <getye2008@gmail.com>
2024-10-15 10:52:57 +0300

    CASL4

--- client/src/AbilityProvider.js
+++ client/src/AbilityProvider.js
@@ -6,6 +6,8 @@ export const AbilityContext = createContext(null);
 export const AbilityProvider = ({ children }) => {
   const userRole = localStorage.getItem('userRole'); 
   const ability = defineAbilitiesFor(userRole); 
+  console.log("Role: ", userRole)
+  console.log("Ability: ", ability)
 
   return (
     <AbilityContext.Provider value={ability}>
