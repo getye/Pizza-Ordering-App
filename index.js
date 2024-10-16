740655aa764b5a33fcd4f4ac8cb20e3ef7d51078
Getye <getye2008@gmail.com>
2024-10-16 16:43:53 +0300

    mobile view Side bar

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use('/', userRouter);
 app.use('/', roleRouter);
 app.use('/', menuRouter);
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
