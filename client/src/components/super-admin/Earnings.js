484e1fca5d1e403b6d49546053a319f98af0646c
Getye <getye2008@gmail.com>
2024-10-17 09:06:36 +0300

    Earnings Report

--- client/src/components/super-admin/Earnings.js
+++ client/src/components/super-admin/Earnings.js
@@ -39,7 +39,7 @@ export const EarningsChart = () => {
   console.log("data: ", data)
 
   return (
-    <Box sx={{ paddingTop:5, width: '98%', height: 400,
+    <Box sx={{ paddingTop:5, height: 400,
       ml: {xs: '5%', sm: '10%', md: '15%', lg: '20%'},
       mr: {xs: '1%', sm: '3%', md: '5%', lg: '7%'},
       mb: {xs: 1, sm: 2, md: 3, lg: 4},
