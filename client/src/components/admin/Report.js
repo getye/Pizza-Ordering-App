0aa187d64b02a75b30ff9d78d83d450642a2449a
Getye <getye2008@gmail.com>
2024-10-17 13:48:32 +0300

    Earning report cahrt

--- client/src/components/admin/Report.js
+++ client/src/components/admin/Report.js
@@ -60,8 +60,9 @@ export const Report = () => {
             ml: {xs: '5%', sm: '10%', md: '15%', lg: '20%'},
             mr: {xs: '1%', sm: '3%', md: '5%', lg: '7%'},
             mb: {xs: 1, sm: 2, md: 3, lg: 4},
+            height: 'auto'
         }}>
-            <ResponsiveContainer width="90%" height={'auto'}>
+            <ResponsiveContainer width="90%" height="100%">
                 <LineChart data={isTotalOrders ? totalOrdersData : totalEarningsData}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="month" />
