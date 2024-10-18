73fb3907d8c400e497c45d316beea2737d05b0cf
Getye <getye2008@gmail.com>
2024-10-18 22:01:06 +0300

    Responsive top bar

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -105,7 +105,7 @@ return (
       </Grid>
 
       <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'end' }}>
-        <img src={home1} alt='Pizza' style={{maxWidth:'65%', maxHeight:'30px'}}/>
+        <img src={home1} alt='Pizza' style={{maxWidth:'65%', maxHeight:'80px'}}/>
         <img src={home2} alt='Pizza' style={{maxWidth:'80%', maxHeight:'80vb'}}/>
       </Grid>
     </Grid>
