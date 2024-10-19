5880f501300a0d8dc255b8b92d7258bfb9d5f18f
Getye <getye2008@gmail.com>
2024-10-19 20:06:06 +0300

    Admin route

--- routes/MenuRoute.js
+++ routes/MenuRoute.js
@@ -12,7 +12,7 @@ menuRouter.get('/customer/view/orders', protect, MenuController.viewOrders);
 menuRouter.get('/manager/view/orders', protect, MenuController.managerViewOrders); 
 menuRouter.get('/manager/view/menus', protect, MenuController.managerViewMenus); 
 menuRouter.put('/kitchen-managr/update/order/status/:order_id', MenuController.updateOrderStatus);
-menuRouter.get('/admin/reports', protect, MenuController.reports); 
+menuRouter.get('/admin/view/reports', protect, MenuController.reports); 
 menuRouter.get('/superadmin/view/earnings', MenuController.viewEarnings); 
 
 
