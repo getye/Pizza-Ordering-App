f73274c8f269e6b1da4fd67d24acc5a7f2659eaa
Getye <getye2008@gmail.com>
2024-11-07 17:14:43 +0300

    Home pizza image

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -79,7 +79,7 @@ return (
       </Grid>
 
       <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'end', position: 'relative' }}>
-        <img src={home1} alt='Pizza' style={{maxWidth:'65%', maxHeight:{xs:'20px', sm:'30px', md:'50', lg:'80px'}}}/>
+        <img src={home1} alt='Pizza' style={{maxWidth:'65%'}}/>
         <img src={home2} alt='Pizza' style={{maxWidth:'80%', maxHeight:'80vb'}}/>
       </Grid>
     </Grid>
