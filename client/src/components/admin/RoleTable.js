95e3d7e44a9f67d66c77094393c425b44fa34893
Getye <getye2008@gmail.com>
2024-11-08 08:44:52 +0300

    Add role

--- client/src/components/admin/RoleTable.js
+++ client/src/components/admin/RoleTable.js
@@ -207,8 +207,8 @@ const handleStatusChange = async (role) => {
 
   return (
     <Box sx={{ paddingTop: 3, 
-      ml: {xs: '1%', sm: '5%', md: '10%', lg: '15%'},
-      mr: {xs: '1%', sm: '5%', md: '10%', lg: '15%'},
+      ml: {xs: '1%', sm: '3%', md: '5%', lg: '7%'},
+      mr: {xs: '1%', sm: '3%', md: '5%', lg: '7%'},
       mb: {xs: 1, sm: 2, md: 3, lg: 4},
      }}>
       <MaterialReactTable
